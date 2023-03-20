d70f35d17993ceccea06d8196276b59cc6ea4107
kooya3 <elyeestatua@gmail.com>
2023-03-20 13:17:58 +0300

    creating the sanity cms schema

---
+++ e-commerce/e-commerce/schemas/product.js
@@ -0,0 +1,42 @@
+export default {
+    namer: 'product',
+    title: 'product',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'array',
+            of: [{
+                type: 'image',
+            }],
+            options: {
+                hotspot: true,
+            },
+        },
+            {
+                name: 'name',
+                title: 'Name',
+                type: 'string',
+            },
+            {
+                name: 'slug',
+                title: 'Slug',
+                type: 'slug',
+                options: {
+                    source: 'name',
+                    maxLength: 90, /* max length of picture */
+                }
+            },
+            {
+                name: 'price',
+                title: 'Price',
+                type: 'number',
+            },
+            {
+                name: 'details',
+                title: 'Details',
+                type: 'string',
+                
+    }]
+}
