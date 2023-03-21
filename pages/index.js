5cc0abeae45b1f53a79f335e936198272ef41790
kooya3 <elyeestatua@gmail.com>
2023-03-21 07:36:43 +0300

    herobanner creation

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,22 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    HeroBanner
 
-    <div>
-      <h2>
-        
-      </h2>
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
     </div>
+
+    <div className="products-container">
+      {products?.map((product) => <Product key={product._id} product={product} />)}
+    </div>
+
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
 
     </>
   )
