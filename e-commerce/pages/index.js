06f66077c8786a5597358b36b9ec4210e14e88b7
kooya3 <elyeestatua@gmail.com>
2023-03-22 23:53:31 +0300

    created components

--- e-commerce/pages/index.js
+++ e-commerce/pages/index.js
@@ -4,19 +4,21 @@ const Home = () => {
   return (
   <>  
   HeroBanner
-    <div>
+
+    <div className="products-heading"> 
       <h2>
         Best Selling Products
       </h2>
       <p>Speakers of many variations</p>
     </div>
 
-    <div>
+    <div className="products-container">
       {[
         'product 1', 'product 2'].map((product) => product)}
     </div>
 
     Footer
+    
   </>
   )
 }
