b60bfa521b4a7ba035801bb1b82e0e4a37721033
kooya3 <elyeestatua@gmail.com>
2023-03-24 01:23:53 +0300

    created the final product starting on success page

--- e-commerce/components/Footer.jsx
+++ e-commerce/components/Footer.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter, AiFillGithub} from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+      <p>2023 🖤kooya3❤️ Headphones All rights reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+        <AiFillGithub />
+      </p>
+    </div>
   )
 }
 
