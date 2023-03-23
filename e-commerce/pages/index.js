c830059d7490483545312ecce93e52bce0f7727e
kooya3 <elyeestatua@gmail.com>
2023-03-23 09:03:56 +0300

    finally linked sanity and nextjs

--- e-commerce/pages/index.js
+++ e-commerce/pages/index.js
@@ -1,37 +1,31 @@
-import React from 'react'
+import React from 'react';
+
 import { client } from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components';
 
-import { Product, FooterBanner, HeroBanner, Footer } from '../components';
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
+    </div>
 
-const Home = () => {
-  return (
-    <>
-      <HeroBanner />
+    <div className="products-container">
+      {products?.map((product) => <Product key={product._id} product={product} />)}
+    </div>
 
-      <div className="products-heading">
-        <h2>
-          Best Selling Products
-        </h2>
-        <p>Speakers of many variations</p>
-      </div>
-
-      <div className="products-container">
-        {[
-          'product 1', 'product 2'].map((product) => product)}
-      </div>
-
-      <FooterBanner />
-
-    </>
-  )
-}
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
 
 export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
   const products = await client.fetch(query);
+
   const bannerQuery = '*[_type == "banner"]';
   const bannerData = await client.fetch(bannerQuery);
-  
+
   return {
     props: { products, bannerData }
   }
